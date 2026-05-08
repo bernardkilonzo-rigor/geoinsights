fafc6aabcf5f83712c6601bd507d8a03aab4c211
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-05-08 12:42:33 +0300

    coloring each point by category.

--- code/Mapping with leaflet/Mapping maps with leaflet.R
+++ code/Mapping with leaflet/Mapping maps with leaflet.R
@@ -8,7 +8,28 @@ library(janitor)
 Facilities <- read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Health%20facilities%20in%20Kibera.csv")%>%
   clean_names()
 
-#creating a basic interactive map
+#creating a basic interactive map (point distribution map)
 leaflet(Facilities)%>%
   addTiles()%>%
-  addMarkers(~longitude, ~latitude, popup = ~name)
+  addMarkers(~longitude,
+             ~latitude,
+             popup = ~name)
+
+#creating color palette
+pal <- colorFactor(
+  palette = c("red","blue"),
+  domain = Facilities$status
+)
+
+#coloring different categories
+leaflet(Facilities)%>%
+  addProviderTiles("CartoDB.Positron")%>%
+  addCircleMarkers(
+    ~longitude,
+    ~latitude,
+    radius = 3,
+    color = ~pal(status),
+    fillOpacity = 0.4,
+    popup = ~paste0("<b>", name, "</b><br>Status: ", status))
+
+#using different icons for different categories
