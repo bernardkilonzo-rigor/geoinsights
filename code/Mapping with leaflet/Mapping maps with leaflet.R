a9e5584552f364831494863d8d5dca055f6f5583
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-05-08 12:10:58 +0300

    creating a basic interactive map with leaflet.

--- code/Mapping with leaflet/Mapping maps with leaflet.R
+++ code/Mapping with leaflet/Mapping maps with leaflet.R
@@ -2,7 +2,13 @@ setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\geoinsights\\code\\Mappi
 #load libraries
 library(sf)
 library(leaflet)
+library(janitor)
 
 #load data set
+Facilities <- read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Health%20facilities%20in%20Kibera.csv")%>%
+  clean_names()
 
-
+#creating a basic interactive map
+leaflet(Facilities)%>%
+  addTiles()%>%
+  addMarkers(~longitude, ~latitude, popup = ~name)
